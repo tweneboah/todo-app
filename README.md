8bb679a1ed821578930cd1be5cfd773eb2d18dc3
Emmanuel <twentekghana@gmail.com>
2018-12-15 08:32:09 +0800

    Filtering and adding todos are added

--- README.md
+++ README.md
@@ -1,3 +1,57 @@
-# todo-app
+# TODO APP
 
-Mark your days with todo's and never forget
+This is an app that keep track of your todo's plan
+
+```javascript
+const todos = [
+  {
+    text: "Teach",
+    completed: false
+  },
+  {
+    text: "Go abroad",
+    completed: true
+  },
+  {
+    text: "Learn React",
+    completed: false
+  },
+  {
+    text: "Birthdat Party",
+    completed: false
+  },
+  {
+    text: "Learn Chines",
+    completed: true
+  }
+];
+
+// Displaying todos left
+
+const incompletedTodos = todos.filter(function(todo) {
+  return !todo.completed;
+});
+
+const createTodoElement = document.createElement("h5");
+createTodoElement.textContent = `Hi you have  ${
+  incompletedTodos.length
+} left todos try to complete`;
+document.querySelector(".todos-left").appendChild(createTodoElement);
+
+//End of Displaying todos left
+
+//Displaying all todos
+
+const DisplayAllTodo = function(todos) {
+  const allTodos = todos.filter(function(todo) {
+    return todo.text;
+  });
+  return allTodos;
+};
+```
+
+### EXPLANATION => STEPS
+
+For me to able to display my data into the page. Since I am not going to use the function in anywhere I will not create a function but rather i will store the return data into a variable.
+
+I use filter() method. filter() returns a new array base on criteria. So my criteria was to return all my todos text.
--- js/todo.js
+++ js/todo.js
@@ -1,43 +1,78 @@
-const todos = [{
-        text: "Teach",
-        completed: false
-    },
-    {
-        text: "Go abroad",
-        completed: true
-    },
-    {
-        text: "Learn React",
-        completed: false
-    },
-    {
-        text: "Birthdat Party",
-        completed: false
-    },
-    {
-        text: "Learn Chines",
-        completed: true
-    }
+const todos = [
+  {
+    text: "Teach",
+    completed: false
+  },
+  {
+    text: "Go abroad",
+    completed: false
+  },
+  {
+    text: "Emmanuel",
+    completed: false
+  },
+  {
+    text: "Learn React",
+    completed: true
+  },
+  {
+    text: "Birthdat Party",
+    completed: false
+  },
+  {
+    text: "Learn Chines",
+    completed: true
+  }
 ];
 
-// Displaying todos left
+let search = "";
 
-const incompletedTodos = todos.filter(function (todo) {
+//RENDERING
+const renderTodos = function(todos, search) {
+  const allTodos = todos.filter(function(todo) {
+    return todo.text.toLowerCase().includes(search.toLowerCase());
+  });
+  document.querySelector("#todos").innerHTML = "";
+  allTodos.forEach(function(todo) {
+    const eL = document.createElement("p");
+    eL.textContent = todo.text;
+    document.querySelector("#todos").appendChild(eL);
+  });
+
+  //Displaying incomplete todos remaining
+  document.querySelector(".todos-left").innerHTML = "";
+  const incompletedTodos = todos.filter(function(todo) {
     return !todo.completed;
-})
+  });
 
-const createTodoElement = document.createElement("h5");
-createTodoElement.textContent = `Hi you have  ${incompletedTodos.length} left todos try to complete`
-document.querySelector(".todos-left").appendChild(createTodoElement);
+  const createTodoElement = document.createElement("h5");
+  createTodoElement.textContent = `Hi you have  ${
+    incompletedTodos.length
+  } left todos try to complete`;
+  document.querySelector(".todos-left").appendChild(createTodoElement);
+  // END OF Displaying incomplete todos remaining
+};
 
-//End of Displaying todos left
+renderTodos(todos, search);
 
+//END OF RENDERING
 
-//Displaying all todos
+//EVENT LISTENER
 
-const DisplayAllTodo = function (todos) {
-    const allTodos = todos.filter(function (todo) {
-        return todo.text;
-    })
-    return allTodos;
-}
+document.querySelector("#search-text").addEventListener("input", function(e) {
+  search = e.target.value;
+  renderTodos(todos, search);
+});
+
+//PUSHING ITEM INO THE ARRAY FOR RENDERING
+
+document.querySelector("#todo-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  todos.push({
+    text: e.target.elements.todoText.value,
+    completed: false
+  });
+  renderTodos(todos, search);
+  e.target.elements.todoText.value = "";
+  console.log(todos);
+});
